cb27e88a5ff9e194faca417220de5bbd6f885757
lambertucci90606 <90606@sistemas.frc.utn.edu.ar>
2023-11-01 21:26:56 -0300

    creacion de proyecto - configuracion de pom.xml

---
+++ servicioEstaciones/src/main/java/bicicletas/servicioEstaciones/ServicioEstacionesApplication.java
@@ -0,0 +1,13 @@
+package bicicletas.servicioEstaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioEstacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioEstacionesApplication.class, args);
+	}
+
+}
---
+++ servicioEstaciones/src/test/java/bicicletas/servicioEstaciones/ServicioEstacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package bicicletas.servicioEstaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioEstacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
